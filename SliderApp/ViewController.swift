f8ca345963ba3138e8b76b1f3eea0823785ff03e
Igor Gadomskii <igvgad@gmail.com>
2022-03-13 08:48:01 +0300

    added sliders and make them change a background color of a view

--- SliderApp/ViewController.swift
+++ SliderApp/ViewController.swift
@@ -1,19 +1,57 @@
-//
-//  ViewController.swift
-//  SliderApp
-//
-//  Created by Macbook on 12.03.2022.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+   
+    @IBAction func changeRedColor() {
+        redSlider.maximumValue = 1.0
+        redSlider.minimumValue = 0.0
+        redSlider.minimumTrackTintColor = .red
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+        viewColor.backgroundColor = UIColor.init(red: redColor,
+                                                 green: greenColor,
+                                                 blue: blueColor,
+                                                 alpha: 1.0)
+        
+    }
+    @IBAction func changeGreenColor() {
+        greenSlider.minimumValue = 0.0
+        greenSlider.maximumValue = 1.0
+        greenSlider.minimumTrackTintColor = .green
+        let greenColor = CGFloat(greenSlider.value)
+        let redColor = CGFloat(redSlider.value)
+        let blueColor = CGFloat(blueSlider.value)
+        viewColor.backgroundColor = UIColor.init(red: redColor,
+                                                 green: greenColor,
+                                                 blue: blueColor,
+                                                 alpha: 1.0)
+        
+        
+    }
+    @IBAction func changeBlueColor() {
+        blueSlider.minimumValue = 0.0
+        blueSlider.maximumValue = 1.0
+        blueSlider.minimumTrackTintColor = .blue
+        let blueColor = CGFloat(blueSlider.value)
+        let redColor = CGFloat(redSlider.value)
+        let greenColor = CGFloat(greenSlider.value)
+        viewColor.backgroundColor = UIColor.init(red: redColor,
+                                                 green: greenColor,
+                                                 blue: blueColor,
+                                                 alpha: 1.0)
+    }
+    
 }
-
